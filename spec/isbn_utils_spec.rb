61b104e418cfcca4cf4e76ba2fd2b5cff8318bd0
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-07 11:33:53 +1200

    filter numbers by index

--- spec/isbn_utils_spec.rb
+++ spec/isbn_utils_spec.rb
@@ -7,8 +7,23 @@ include IsbnUtils
 RSpec.describe IsbnUtils do
   context '.to_digits' do
     subject { 123_456_789_012 }
+
     it 'returns array with digits' do
       expect(to_digits(subject)).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2])
+    end
+  end
+
+  context '.odd_even_indexes_arrays' do
+    let!(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2] }
+
+    subject { odd_even_indexes_arrays(array) }
+
+    it 'odd_inexes_array has odd numbers' do
+      expect(subject[:odd_inexes_array]).to eq([2, 4, 6, 8, 0, 2])
+    end
+
+    it 'even_inexes_array has even numbers' do
+      expect(subject[:even_inexes_array]).to eq([1, 3, 5, 7, 9, 1])
     end
   end
 end
--- src/isbn_utils.rb
+++ src/isbn_utils.rb
@@ -2,4 +2,14 @@ module IsbnUtils
   def to_digits(number)
     number.to_s.split('').map(&:to_i)
   end
+
+  def odd_even_indexes_arrays(array)
+    odd_inexes_array = array.select.with_index { |_item, idx| idx.odd? }
+    even_inexes_array = array.select.with_index { |_item, idx| idx.even? }
+
+    {
+      odd_inexes_array: odd_inexes_array,
+      even_inexes_array: even_inexes_array
+    }
+  end
 end
